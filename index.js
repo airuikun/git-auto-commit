01d338a813af000ed0ae3d9755d0f1f7388315be
ruikunai <ruikunai@tencent.com>
2019-05-12 18:06:58 +0800

    【feature】提交定时器done

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const fs = require('fs-extra')
 const dateFns = require('date-fns')
 const git = require('simple-git');
+const path = require('path')
 
+//需要配置的信息
 const dir = `/Users/airuikun/Desktop/project/technology/git-auto-commit/mongoose_crud`
-// const dirRM = `/Users/airuikun/Desktop/project/technology/git-auto-commit/mongoose_crud/README.md`
-const dirRM = `/Users/airuikun/Desktop/project/technology/git-auto-commit/mongoose_crud/recordLeah.js`
+const dirRM = path.join(dir, `/recordLeah.js`)
 
 const user = '第一名的小蝌蚪'
 const email = '30694912@qq.com'
+//初始时间
+const startTime = '2018-10-13'
+//所要提交的次数
+const loop = 3
 
 execProcess()
 
@@ -17,19 +22,23 @@ async function execProcess() {
 
 async function writeFile() {
     try {
-        const record = dateFns.format(new Date(), 'YYYY/MM/DD hh:mm:ss')
+        const gitTarget = git(dir);
+        gitTarget.pull('origin', 'master')
         //判断是否存在recordLeah.js文件 没有则创建
         await fs.ensureFile(dirRM)
-
-        let readData = await fs.readFile(dirRM, 'utf-8')
-        await fs.appendFile(dirRM, `git auto commit: ${record} \n`)
-        git(dir)
-            // .addConfig('user.name', user)
-            // .addConfig('user.email', email)
-            .add('./*')
-            .commit(`git auto commit: ${record}`, { '--author': `"${user} <${email}>"`, '--date': `Sun Sep 4 08:00:00 2018 +0800`})
-            // .push('origin', 'master')  
-        console.log(`提交成功：${record}`)
+        for (let i = 0; i < loop; i++) {
+            const initTime = (new Date(startTime)).getTime() + (1*24*60*60*1000)*i//得到目标时间毫秒数
+            const targetTime = new Date(initTime)//得到目标时间GMT值
+            const record = dateFns.format(new Date(initTime), 'YYYY/MM/DD hh:mm:ss')//得到目标时间的具体格式的值
+            //添加内容
+            await fs.appendFile(dirRM, `git auto commit: ${record} \n`)
+            //一定要控制好异步操作同步化 要不然会只有一个commit
+            await gitTarget
+                .add('./*')
+                .commit(`git auto commit: ${record}`, { '--author': `"${user} <${email}>"`, '--date': `${targetTime}`})
+        }
+        //多个commit合并成一次提交
+        gitTarget.push('origin', 'master')
     } catch (error) {
         console.log('error', JSON.stringify(error))
     }
